de803a52f8f839868b1164837a4e922bdbf598e0
nhtY <n_hat.yalcin@outlook.com>
2024-02-04 22:39:45 +0300

    add search methods for flight

--- src/main/java/com/nihat/flightsearchapi/repositories/FlightRepository.java
+++ src/main/java/com/nihat/flightsearchapi/repositories/FlightRepository.java
@@ -3,5 +3,14 @@ package com.nihat.flightsearchapi.repositories;
 import com.nihat.flightsearchapi.entities.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface FlightRepository extends JpaRepository<Flight, Long> {
+    List<Flight> findByDepartureAirportCityAndArrivalAirportCityAndDepartureDateTimeAfterAndReturnDateTimeIsNull(
+            String departureCity, String arrivalCity, LocalDateTime departureDateTime);
+
+    List<Flight> findByDepartureAirportCityAndArrivalAirportCityAndDepartureDateTimeAfterAndReturnDateTimeNotNull(
+            String departureCity, String arrivalCity, LocalDateTime departureDateTime);
+
 }
