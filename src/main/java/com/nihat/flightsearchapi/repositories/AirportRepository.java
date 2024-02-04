6ee188c1a21220b65fafe9e61f046b0517d154fe
nhtY <n_hat.yalcin@outlook.com>
2024-02-04 23:23:01 +0300

    add findByCity method: - for fixing the flight updates

--- src/main/java/com/nihat/flightsearchapi/repositories/AirportRepository.java
+++ src/main/java/com/nihat/flightsearchapi/repositories/AirportRepository.java
@@ -3,5 +3,8 @@ package com.nihat.flightsearchapi.repositories;
 import com.nihat.flightsearchapi.entities.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AirportRepository extends JpaRepository<Airport, Long> {
+    Optional<Airport> findByCity(String departureCity);
 }
